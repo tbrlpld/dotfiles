12a3bd06d2cffd119fd87b06424017b3205bb2d3
Tibor Leupold <tibor@lpld.io>
2020-05-24 16:44:56 -1000

    List directories first

--- aliases.sh
+++ aliases.sh
@@ -1,7 +1,7 @@
 # alias ls='ls -FG' # F - extended markup (/ after directory, * after executable), G - colorized output.
 # alias ll='ls -lh' # h - human readable file sizes
 alias ls='/usr/local/bin/gls -p --color'
-alias ll='ls -lh --time-style=long-iso'
+alias ll='ls -lh --time-style=long-iso --group-directories-first'
 alias la='ll -a'
 
 # Python virtual environments
