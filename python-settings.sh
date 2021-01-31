7c24963e5e1906d1dbb211f62449b9f2e344b28b
Tibor Leupold <tibor.leupold@torchbox.com>
2021-01-30 16:36:39 -1000

    Remove python path settings for 3.6
    The linked Python version is not available with macOS Big Sur anymore.
    The links where broken.

--- python-settings.sh
+++ python-settings.sh
@@ -1,6 +1,3 @@
-# Setting PATH for Python 3.6
-# The original version is saved in .bash_profile.pysave
-export PATH="/Library/Frameworks/Python.framework/Versions/3.6/bin:${PATH}"
 # Disable creation of .pyc (byte code files)
 export PYTHONDONTWRITEBYTECODE=1
 
@@ -10,10 +7,6 @@ fi
 
 # Argcomplete autocompletion
 source ~/.bash_completion.d/python-argcomplete.sh
-
-# For pipx (to install isolated Python applications)
-export PATH="$PATH:/Users/tibor/Library/Python/3.6/bin"
-eval "$(register-python-argcomplete /Users/tibor/Library/Python/3.6/bin/pipx)"
 
 
 # Pip requires to have an active virtual environment to install packages
