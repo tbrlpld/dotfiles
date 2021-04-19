0cf10ca26fabc6ba33a299555eda11df3469467c
Tibor Leupold <tibor.leupold@torchbox.com>
2021-04-18 23:36:43 -1000

    Comment all compiler flags
    This is not like the PATH. These flags are not being extended, they are
    only overridden. So these flags need to be set manually when you run into
    issues here.

--- brew.sh
+++ brew.sh
@@ -1,29 +1,31 @@
 # Homebrew Install Caveats
 
-# For compilers to find libffi you may need to set:
-export LDFLAGS="-L/usr/local/opt/libffi/lib"
-export CPPFLAGS="-I/usr/local/opt/libffi/include"
-# For pkg-config to find libffi you may need to set:
-export PKG_CONFIG_PATH="/usr/local/opt/libffi/lib/pkgconfig"
+# # For compilers to find libffi you may need to set:
+# export LDFLAGS="-L/usr/local/opt/libffi/lib"
+# export CPPFLAGS="-I/usr/local/opt/libffi/include"
+# # For pkg-config to find libffi you may need to set:
+# export PKG_CONFIG_PATH="/usr/local/opt/libffi/lib/pkgconfig"
 
 
-# For compilers to find openssl@1.1 you may need to set:
-export LDFLAGS="-L/usr/local/opt/openssl@1.1/lib"
-export CPPFLAGS="-I/usr/local/opt/openssl@1.1/include"
-# For pkg-config to find openssl@1.1 you may need to set:
-export PKG_CONFIG_PATH="/usr/local/opt/openssl@1.1/lib/pkgconfig"
+# # For compilers to find openssl@1.1 you may need to set:
+# export LDFLAGS="-L/usr/local/opt/openssl@1.1/lib"
+# export CPPFLAGS="-I/usr/local/opt/openssl@1.1/include"
+# export LDFLAGS="-L/usr/local/opt/openssl/lib"
+# export CPPFLAGS="-I/usr/local/opt/openssl/include"
+# # For pkg-config to find openssl@1.1 you may need to set:
+# export PKG_CONFIG_PATH="/usr/local/opt/openssl@1.1/lib/pkgconfig"
 
-# For compilers to find readline you may need to set:
-export LDFLAGS="-L/usr/local/opt/readline/lib"
-export CPPFLAGS="-I/usr/local/opt/readline/include"
-# For pkg-config to find readline you may need to set:
-export PKG_CONFIG_PATH="/usr/local/opt/readline/lib/pkgconfig"
+# # For compilers to find readline you may need to set:
+# export LDFLAGS="-L/usr/local/opt/readline/lib"
+# export CPPFLAGS="-I/usr/local/opt/readline/include"
+# # For pkg-config to find readline you may need to set:
+# export PKG_CONFIG_PATH="/usr/local/opt/readline/lib/pkgconfig"
 
-# For compilers to find sqlite you may need to set:
-export LDFLAGS="-L/usr/local/opt/sqlite/lib"
-export CPPFLAGS="-I/usr/local/opt/sqlite/include"
-# For pkg-config to find sqlite you may need to set:
-export PKG_CONFIG_PATH="/usr/local/opt/sqlite/lib/pkgconfig"
+# # For compilers to find sqlite you may need to set:
+# export LDFLAGS="-L/usr/local/opt/sqlite/lib"
+# export CPPFLAGS="-I/usr/local/opt/sqlite/include"
+# # For pkg-config to find sqlite you may need to set:
+# export PKG_CONFIG_PATH="/usr/local/opt/sqlite/lib/pkgconfig"
 
 # NVM
 export NVM_DIR="$HOME/.nvm"
