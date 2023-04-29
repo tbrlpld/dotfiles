9e7835612ac4e8da118c6f1ec56cea6353b0053f
Tibor Leupold <tibor@lpld.io>
2023-04-29 08:22:07 -0700

    Make prompt bold and dir blue

--- prompt.zsh
+++ prompt.zsh
@@ -1,3 +1,3 @@
 source ~/.dotfiles/git-prompt.sh
 GIT_PS1_SHOWDIRTYSTATE=true
-setopt PROMPT_SUBST ; PS1='%9c%{%F{green}%}$(__git_ps1 " (%s)")%{%F{none}%} $ '
+setopt PROMPT_SUBST ; PS1='%{%F{blue}%}%B%9c%{%F{green}%}$(__git_ps1 " (%s)")%{%F{none}%} $ %b'
