60f7ae3a2c4a5c82f2d4a5f1d9f4068802cbba7b
Tibor Leupold <tibor@lpld.io>
2023-04-29 00:05:11 -0700

    Add git prompt

--- .zshrc
+++ .zshrc
@@ -3,6 +3,7 @@ source ~/.dotfiles/brew.sh
 source ~/.dotfiles/editor.sh
 source ~/.dotfiles/python-settings.sh
 source ~/.dotfiles/paths.sh
+source ~/.dotfiles/prompt.zsh
 
 if type "heroku" > /dev/null; then
     $(heroku autocomplete:script zsh >> /dev/null)
--- prompt.zsh
+++ prompt.zsh
@@ -1,12 +1,16 @@
-parse_git_branch() {
-    # git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
-    git symbolic-ref --short HEAD 2> /dev/null
-}
-git_prompt() {
- ref=$(git symbolic-ref --short HEAD 2> /dev/null)
- echo " ($ref)"
-}
-
-setopt PROMPT_SUBST
+## https://thepugautomatic.com/2008/12/git-dirty-prompt/
+##
+#parse_git_dirty() {
+#  if [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit, working tree clean" ]]; then
+#      echo "*"
+#  fi
+#}
+## https://stackoverflow.com/a/56178134
+#parse_git_branch() {
+#    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1 $(parse_git_dirty) )/'
+#}
+# setopt PROMPT_SUBST
 # PROMPT='%9c%{%F{green}%}$(parse_git_branch)%{%F{none}%} $ '
-PROMPT='%9c%{%F{green}%}$(git_prompt)%{%F{none}%} $ '
+source ~/.dotfiles/git-prompt.sh
+GIT_PS1_SHOWDIRTYSTATE=true
+setopt PROMPT_SUBST ; PS1='%9c% $(__git_ps1 " (%s)") $ '
