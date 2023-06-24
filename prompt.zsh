fc0821277f61a195d10948bd67b45746cb2f9cf0
Tibor Leupold <tibor@lpld.io>
2023-06-24 10:05:50 -0700

    Use linebreaks in prompt

--- prompt.zsh
+++ prompt.zsh
@@ -1,3 +1,3 @@
 source ~/.dotfiles/git-prompt.sh
 GIT_PS1_SHOWDIRTYSTATE=true
-setopt PROMPT_SUBST ; PS1='%{%F{blue}%}%B%9c%{%F{green}%}$(__git_ps1 " (%s)")%{%F{none}%} $ %b'
+setopt PROMPT_SUBST ; PS1=$'\n''%{%F{blue}%}%B%9c%{%F{green}%}$(__git_ps1 " (%s)")%{%F{none}%} '$'\n''%% %b'
