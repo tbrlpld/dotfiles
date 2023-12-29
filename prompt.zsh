3232c523b19643286fa6bb4c66ce8281861d44e5
Tibor Leupold <tibor@lpld.io>
2023-12-29 11:43:30 -0800

    Custom promopt for Python venv to show in one line

--- prompt.zsh
+++ prompt.zsh
@@ -1,3 +1,23 @@
 source ~/.dotfiles/git-prompt.sh
 GIT_PS1_SHOWDIRTYSTATE=true
-setopt PROMPT_SUBST ; PS1=$'\n''%{%F{blue}%}%B%9c%{%F{green}%}$(__git_ps1 " (%s)")%{%F{none}%} '$'\n''%% %b'
+
+# Custom Python virtual env prompt.
+#
+# The default one adds it self directly before the normal prompt. But mine starts with a linebreak. This means that the venv prompt is on one line and the directory and git info on another.
+function virtualenv_info(){
+    # Get Virtual Env
+    if [[ -n "$VIRTUAL_ENV" ]]; then
+        # Strip out the path and just leave the env name
+        venv="${VIRTUAL_ENV##*/}"
+    else
+        # In case you don't have one activated
+        venv=''
+    fi
+    [[ -n "$venv" ]] && echo "($venv) "
+}
+# disable the default virtualenv prompt change
+export VIRTUAL_ENV_DISABLE_PROMPT=1
+VENV="\$(virtualenv_info)";
+
+setopt PROMPT_SUBST ; PS1=$'\n'$VENV'''%{%F{blue}%}%B%9c%{%F{green}%}$(__git_ps1 " (%s)")%{%F{none}%} '$'\n''%% %b'
+
